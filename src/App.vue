585368b4b3e199dd5580d1e1aa18e6dce66d6c08
Stephen Brown <steve@brown.bg>
2024-10-05 19:25:39 +0200

    Add link to download

--- src/App.vue
+++ src/App.vue
@@ -70,10 +70,11 @@
                 Stop logging work manually. Track your work as you do with the easy desktop app and ensure accuracy
               </p>
               <div class="mt-10 flex items-center justify-center gap-x-6">
-                <a href="#"
-                  class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
-                  started</a>
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span
+                <a href="public/WorklogAssistant-windows-0.0.5.zip"
+                  class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  Get started
+                </a>
+                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more<span
                     aria-hidden="true">→</span></a>
               </div>
             </div>
